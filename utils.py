b23c69c330cd03345d00c1252acac5f0dfc6fdf8
ttravv <alegofnik@gmail.com>
2024-04-07 16:18:44 +0300

    cleared

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
-baida = 'page=1&limit=10&sortField=rating.imdb&sortType=-1&type=movie&votes.imdb=100000-6666666666666'
+sort = 'page=1&limit=10&sortField=rating.imdb&sortType=-1&type=movie&votes.imdb=100000-6666666666666'
 
-def test(baida: str) -> dict[str, str]:
+def test(sort: str) -> dict[str, str]:
     new_dict = {}
-    for i in baida.split('&'):
+    for i in sort.split('&'):
         i_split = i.split('=')
         new_dict[i_split[0]] = i_split[1]
     return new_dict
 
-print(test(baida))
+print(test(sort))
     
 
 
